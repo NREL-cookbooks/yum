48cc946a858c22d1676187ba5778d7a43a1341a1
Muerdter, Nick <nick.muerdter@nrel.gov>
2010-12-03 07:59:29 +0000

    Adding cookbook for adding the EPEL repo.

---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: yum
+# Recipe:: default
+#
+# Copyright 2010, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
---
+++ recipes/epel.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: yum
+# Recipe:: epel
+#
+# Copyright 2010, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+remote_file "/tmp/epel-release-6-5.noarch.rpm" do
+  source "http://download.fedora.redhat.com/pub/epel/beta/6/i386/epel-release-6-5.noarch.rpm"
+  checksum "28aacc55514d080d0813b1598c4a6a708b064bef0a373aacb60eaa16998d157b"
+end
+
+package "epel-release" do
+  source "/tmp/epel-release-6-5.noarch.rpm"
+  options "--nogpgcheck" 
+end
