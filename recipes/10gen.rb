81919d756349b212bbad9fb7acd4f61a0084785a
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-03-30 20:13:51 +0000

    Add recipe to install the 10gen yum repo.

---
+++ recipes/10gen.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: yum
+# Recipe:: 10gen
+#
+# Copyright 2010, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if platform?("redhat", "centos", "fedora")
+  template "/etc/yum.repos.d/10gen.repo"
+    source "10gen.repo.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables({
+      :platform => node[:kernel][:machine]
+    })
+  end
+end
