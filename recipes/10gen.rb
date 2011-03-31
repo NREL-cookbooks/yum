53cb39c9acdff90a584071226dd3d1ddf87374b9
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-03-31 02:00:26 +0000

    Fix 10gen repo.

--- recipes/10gen.rb
+++ recipes/10gen.rb
@@ -8,7 +8,7 @@
 #
 
 if platform?("redhat", "centos", "fedora")
-  template "/etc/yum.repos.d/10gen.repo"
+  template "/etc/yum.repos.d/10gen.repo" do
     source "10gen.repo.erb"
     owner "root"
     group "root"
