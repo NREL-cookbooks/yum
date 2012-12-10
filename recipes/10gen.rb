3f43cc6c3d99850709c1d9c9e00c860c99c0afd9
Nick Muerdter <nick.muerdter@nrel.gov>
2012-12-09 18:02:41 -0700

    Fix 10gen URL for 32 bit systems.

--- recipes/10gen.rb
+++ recipes/10gen.rb
@@ -8,9 +8,10 @@
 #
 
 if platform?("redhat", "centos", "fedora")
+  arch = if(node[:kernel][:machine] =~ /x86_64/) then "x86_64" else "i686" end
   yum_repository "10gen" do
     action :add
     description "10gen Repository"
-    url "http://downloads-distro.mongodb.org/repo/redhat/os/$basearch"
+    url "http://downloads-distro.mongodb.org/repo/redhat/os/#{arch}"
   end
 end
