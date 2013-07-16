b11e78dedcb63807c1099ba454c68e559998a7cb
Nick Muerdter <nick.muerdter@nrel.gov>
2013-07-15 22:02:00 -0600

    Install latest repoforge.

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,7 +22,7 @@
 default['yum']['exclude'] = Array.new
 default['yum']['installonlypkgs'] = Array.new
 default['yum']['ius_release'] = '1.0-11'
-default['yum']['repoforge_release'] = '0.5.2-2'
+default['yum']['repoforge_release'] = '0.5.3-1'
 default['yum']['proxy'] = ''
 default['yum']['proxy_username'] = ''
 default['yum']['proxy_password'] = ''
