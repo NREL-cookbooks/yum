a92aed516f8e1272e768b17fc1c96ac1ca6dd845
Nick Muerdter <nick.muerdter@nrel.gov>
2013-07-30 00:04:13 -0600

    Change yum repo to point to EL6 URL.

--- attributes/varnish.rb
+++ attributes/varnish.rb
@@ -1,1 +1,1 @@
-default['yum']['varnish']['url'] = "http://repo.varnish-cache.org/redhat/varnish-3.0/el5/$basearch"
+default['yum']['varnish']['url'] = "http://repo.varnish-cache.org/redhat/varnish-3.0/el6/$basearch"
